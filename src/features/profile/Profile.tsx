fb76bdf72c2075198b7981fc7756cfb9907b747d
Minh Sáng <minhsang.1mil@gmail.com>
2026-01-04 12:35:39 +0700

    refactor: ui profile page

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -104,139 +104,146 @@ export default function Profile() {
   }
 
   return (
-    <div className="profile-page p-5 w-full md:w-6/10 lg:w-5/10">
-      <div className="profile-header">
-        <div className="relative flex-none">
-          <img
-            src={userProfile?.avatar! ?? defaultAvatarPath}
-            alt="Avatar"
-            className="rounded-full w-30"
-          />
-          <label className="edit-icon absolute sm:bottom-0 sm:right-0 bg-blue-300 w-6 h-6 sm:w-7 sm:h-7 rounded-full flex items-center justify-center cursor-pointer shadow-md">
+    <div
+      style={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div className="w-full px-4 sm:px-0 sm:max-w-md md:max-w-lg 2xl:max-w-2xl mt-3 mx-auto">
+        <div className="flex items-center gap-4 mb-3 p-5">
+          {/* Avatar */}
+          <div className="relative shrink-0">
             <img
-              src="/icons/edit-icon.png"
-              alt="Edit"
-              className="w-3 h-3 sm:w-4 sm:h-4 object-contain"
+              src={userProfile?.avatar ?? defaultAvatarPath}
+              alt="Avatar"
+              className="w-20 rounded-full object-cover"
             />
-            <input
-              type="file"
-              accept="image/*"
-              hidden
-              onChange={handleFileChange}
+
+            <label className="absolute bottom-0 right-1 flex h-5.5 w-5.5 items-center justify-center rounded-full bg-blue-500 shadow cursor-pointer">
+              <img src="/icons/edit-icon.png" className="h-3 w-3" />
+              <input type="file" hidden />
+            </label>
+          </div>
+
+          {/* Name */}
+          <div className="flex flex-col justify-center">
+            <h2 className="text-2xl font-semibold leading-tight">Chloe Kim</h2>
+          </div>
+        </div>
+
+        <form className="profile-form" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="field">
+              <label>First Name</label>
+              <input
+                name="firstName"
+                value={userProfile?.firstName}
+                onChange={(e) =>
+                  setUserProfile((pre) => ({
+                    ...pre,
+                    firstName: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div className="field">
+              <label>Last Name</label>
+              <input
+                name="lastName"
+                value={userProfile?.lastName}
+                onChange={(e) =>
+                  setUserProfile((pre) => ({
+                    ...pre,
+                    lastName: e.target.value,
+                  }))
+                }
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="field">
+              <label>Email Address</label>
+              <input
+                name="email"
+                type="email"
+                value={userProfile?.email}
+                onChange={(e) =>
+                  setUserProfile((pre) => ({ ...pre!, email: e.target.value }))
+                }
+              />
+            </div>
+            <div className="field">
+              <label>Phone Number</label>
+              <input
+                name="phone"
+                value={userProfile?.phoneNumber}
+                onChange={(e) =>
+                  setUserProfile((pre) => ({
+                    ...pre!,
+                    phoneNumber: e.target.value,
+                  }))
+                }
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="field">
+              <label>Date of Birth</label>
+              <DateInput
+                value={userProfile?.dateOfBirth}
+                onChange={(date) => {
+                  console.log("🚀 ~ profilePage ~ date:", date);
+
+                  setUserProfile((pre) => ({
+                    ...pre,
+                    dateOfBirth: date ?? undefined,
+                  }));
+                }}
+                allowDeselect
+                maxDate={new Date(new Date().getFullYear() - 18, 11, 31)}
+                minDate={new Date(new Date().getFullYear() - 100, 0, 1)}
+                valueFormat="DD/MM/YYYY"
+                placeholder="Date of Birth"
+              />
+            </div>
+
+            <div className="field">
+              <label>Gender</label>
+              <Select
+                styles={customStyles}
+                options={genderOptions}
+                value={
+                  userProfile.gender
+                    ? {
+                        value: userProfile.gender,
+                        label: Gender[userProfile.gender],
+                      }
+                    : null
+                }
+                onChange={(option) => {
+                  setUserProfile((prev) => ({
+                    ...prev,
+                    gender: option?.value as Gender,
+                  }));
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="btn-wrapper">
+            <LoadingButton
+              loading={loading}
+              text="Save Changes"
+              type="submit"
+              className="save-btn"
             />
-          </label>
-        </div>
-        <div className="profile-info flex-1 min-w-0 pl-2 md:pl-1">
-          <h2>
-            {userProfile?.firstName} {userProfile?.lastName}
-          </h2>
-        </div>
+          </div>
+        </form>
       </div>
-
-      <form className="profile-form" onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="field">
-            <label>First Name</label>
-            <input
-              name="firstName"
-              value={userProfile?.firstName}
-              onChange={(e) =>
-                setUserProfile((pre) => ({ ...pre, firstName: e.target.value }))
-              }
-            />
-          </div>
-          <div className="field">
-            <label>Last Name</label>
-            <input
-              name="lastName"
-              value={userProfile?.lastName}
-              onChange={(e) =>
-                setUserProfile((pre) => ({ ...pre, lastName: e.target.value }))
-              }
-            />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="field">
-            <label>Email Address</label>
-            <input
-              name="email"
-              type="email"
-              value={userProfile?.email}
-              onChange={(e) =>
-                setUserProfile((pre) => ({ ...pre!, email: e.target.value }))
-              }
-            />
-          </div>
-          <div className="field">
-            <label>Phone Number</label>
-            <input
-              name="phone"
-              value={userProfile?.phoneNumber}
-              onChange={(e) =>
-                setUserProfile((pre) => ({
-                  ...pre!,
-                  phoneNumber: e.target.value,
-                }))
-              }
-            />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="field">
-            <label>Date of Birth</label>
-            <DateInput
-              value={userProfile?.dateOfBirth}
-              onChange={(date) => {
-                console.log("🚀 ~ profilePage ~ date:", date);
-
-                setUserProfile((pre) => ({
-                  ...pre,
-                  dateOfBirth: date ?? undefined,
-                }));
-              }}
-              allowDeselect
-              maxDate={new Date(new Date().getFullYear() - 18, 11, 31)}
-              minDate={new Date(new Date().getFullYear() - 100, 0, 1)}
-              valueFormat="DD/MM/YYYY"
-              placeholder="Date of Birth"
-            />
-          </div>
-
-          <div className="field">
-            <label>Gender</label>
-            <Select
-              styles={customStyles}
-              options={genderOptions}
-              value={
-                userProfile.gender
-                  ? {
-                      value: userProfile.gender,
-                      label: Gender[userProfile.gender],
-                    }
-                  : null
-              }
-              onChange={(option) => {
-                setUserProfile((prev) => ({
-                  ...prev,
-                  gender: option?.value as Gender,
-                }));
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="btn-wrapper">
-          <LoadingButton
-            loading={loading}
-            text="Save Changes"
-            type="submit"
-            className="save-btn"
-          />
-        </div>
-      </form>
     </div>
   );
 }
